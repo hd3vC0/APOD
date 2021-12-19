04d1a053497250c79722da3beabe31da25ea8d08
Humberto M <humbertomonterrosa@gmail.com>
2021-12-19 18:35:52 -0500

    added activity indicator while image loading

--- APOD/ViewControllers/PreviewViewController.m
+++ APOD/ViewControllers/PreviewViewController.m
@@ -14,7 +14,9 @@
 @property (weak, nonatomic) IBOutlet UILabel *date;
 @property (weak, nonatomic) IBOutlet UILabel *textDescription;
 @property (weak, nonatomic) IBOutlet UILabel *textTitle;
+@property (weak, nonatomic) IBOutlet UIStackView *downloadIndicator;
 
+- (void)imageDownloadCompleted;
 @end
 
 @implementation PreviewViewController
@@ -22,7 +24,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     UIImage *placeholder = [UIImage imageNamed:@"placeholder"];
-    [self.imagePreview setImageWithURL:[NSURL URLWithString:self.astronomyPicture.picture] placeholderImage:placeholder];
+    //[self.imagePreview setImageWithURL:[NSURL URLWithString:self.astronomyPicture.picture] placeholderImage:placeholder];
+    
+    [self.imagePreview setImageWithURLRequest:[[NSURLRequest alloc]initWithURL:[NSURL URLWithString:self.astronomyPicture.picture]] placeholderImage:placeholder success:^(NSURLRequest * _Nonnull request, NSHTTPURLResponse * _Nullable response, UIImage * _Nonnull image) {
+        [self imageDownloadCompleted];
+        self.imagePreview.image = image;
+    } failure:^(NSURLRequest * _Nonnull request, NSHTTPURLResponse * _Nullable response, NSError * _Nonnull error) {        
+        [self imageDownloadCompleted];
+    }];
     [self.date setText:self.astronomyPicture.date];
     [self.textDescription setText:self.astronomyPicture.textDescription];
     [self.textTitle setText:self.astronomyPicture.title];
@@ -30,5 +39,9 @@
 
 - (IBAction)closeButton:(id)sender {
     [self dismissViewControllerAnimated:TRUE completion:nil];
+}
+
+-(void)imageDownloadCompleted{
+    [self.downloadIndicator setHidden:TRUE];
 }
 @end

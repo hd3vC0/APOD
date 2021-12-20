c663eceb15576ae96aa0dc357303474c527a5170
Humberto M <humbertomonterrosa@gmail.com>
2021-12-19 21:05:42 -0500

    Add animation to the image after it has been loaded

--- APOD/ViewControllers/PictureCollectionViewCell.m
+++ APOD/ViewControllers/PictureCollectionViewCell.m
@@ -11,6 +11,7 @@
 @interface PictureCollectionViewCell()
 @property (weak, nonatomic) IBOutlet UILabel *date;
 @property (weak, nonatomic) IBOutlet UIImageView *image;
+-(void) animate;
 @end
 
 @implementation PictureCollectionViewCell
@@ -22,8 +23,27 @@
 -(void)setData:(AstronomyPicture *)astronomyPicture{
     [self setAstronomyPicture:astronomyPicture];
     UIImage * placeholder = [UIImage imageNamed:@"placeholder"];
-    [self.image setImageWithURL:[NSURL URLWithString:astronomyPicture.thumbnail] placeholderImage:placeholder];
+    //[self.image setImageWithURL:[NSURL URLWithString:astronomyPicture.thumbnail] placeholderImage:placeholder];
+    
+    [self.image setImageWithURLRequest:[[NSURLRequest alloc] initWithURL:[NSURL URLWithString:astronomyPicture.thumbnail]] placeholderImage:placeholder success:^(NSURLRequest * _Nonnull request, NSHTTPURLResponse * _Nullable response, UIImage * _Nonnull image) {
+        self.image.image = image;
+        [self animate];
+        
+    } failure:^(NSURLRequest * _Nonnull request, NSHTTPURLResponse * _Nullable response, NSError * _Nonnull error) {
+        
+    }];
     [self.date setText:astronomyPicture.date];
+}
+
+- (void)animate{
+    CABasicAnimation *anim = [[CABasicAnimation alloc] init];
+    anim.keyPath = @"opacity";
+    anim.fromValue = @0.0;
+    anim.toValue = @1.0;
+    anim.duration = 0.5;
+    anim.removedOnCompletion = NO;
+    
+    [self.image.layer addAnimation:anim forKey:@"opacity"];
 }
 
 @end
